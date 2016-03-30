8d2dc8a4c4db7828adf1e99e47c9e394298936bf
Giancarlo Alves <giancarlo.alves96@gmail.com>
2016-03-30 09:00:46 -0300

    Restricted access to attributes in Time

--- src/Time.java
+++ src/Time.java
@@ -2,12 +2,41 @@ public class Time {
 	int hour;
 	int minute;
 	
-	Time(){}
+	Time(){
+		hour=0;
+		minute=0;
+	}
+	
+	public int getHour(){
+		return this.hour;
+	}
+	
+	public int getMinute(){
+		return this.minute;
+	}
+	
+	public void setHour(int hour){
+		this.hour=hour;
+	}
+	
+	public void setMinute(int minute){
+		this.minute=minute;
+	}
+	
+	String getTime(){
+		int hour = getHour();
+		int minute = getMinute();
+		String time = "";
+		time+=hour;
+		time+=":";
+		time+=minute;
+		return time;
+	}
 	
 	public static void main(String[] args) {
 		Time t = new Time();
-		t.hour=3;
-		t.minute=25;
-		System.out.println("The time now is "+t.hour+":"+t.minute);
+		t.setHour(3);
+		t.setMinute(25);
+		System.out.println("The time now is "+t.getTime());
 	}
 }
